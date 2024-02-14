622e1b9b0bfa3ce8833ada1e2f99d5e4c7c1ecde
remel-crowe <remelscrowe@gmail.com>
2024-02-14 00:28:45 +0000

    initial commit

---
+++ gameboard.js
@@ -0,0 +1,14 @@
+class GameBoard {
+  constructor() {}
+
+  createBoard(size) {
+    const board = [];
+    for (let i = 0; i < size; i++) {}
+  }
+
+  placeship(x, y) {}
+
+  receiveAttack(x, y) {}
+
+  allSunk() {}
+}
---
+++ ship.js
@@ -0,0 +1,15 @@
+class ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    return (this.hits += 1);
+  }
+
+  isSunk() {
+    return this.hits === this.length;
+  }
+}
